441edd0d314d101f552acb68b1e6dd7df93dcdc3
Olga Lipina <neon_alfa@mail.ru>
2023-04-09 18:54:59 +0300

    Updated attention, add intercative plots, multiplots for multilayers, gaussian noise.

--- src/image_transforms.py
+++ src/image_transforms.py
@@ -28,6 +28,18 @@ def gauss_noise_tensor(img):
     return out
 
 
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
+
 def random_sharpness_or_blur(img):
     """Randomly make image more shurp or blurry."""
     rand_trans = [transforms.RandomAdjustSharpness(
